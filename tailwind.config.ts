bc4f90d36a750642343aa5b9f1a6ad522b362173
WooLegend <dnwogus4260@naver.com>
2025-01-15 16:15:14 +0900

    브랜치 생성 액션 및 eslint 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,10 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
   darkMode: ['class'],
-  content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: {
