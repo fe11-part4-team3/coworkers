3a0df8f64c75b5191f014b3b917a454a143a0848
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-16 15:33:04 +0900

    🔨 Container 컴포넌트 사용 예제

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Container from '@/components/layout/Container';
+
 export default function LandingPage() {
-  return <div>랜딩 페이지</div>;
+  return (
+    <Container>
+      <div>랜딩 페이지</div>
+    </Container>
+  );
 }
