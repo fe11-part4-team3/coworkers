7acb7bc82e302ce3a4d3e851c60524e18ec34527
HwichulShin <shinhwiiron@gmail.com>
2025-01-17 21:11:06 +0900

    💄 드롭다운 버튼 ui 추가
    드롭다운 공통 컴포넌트 개발되면 추가 작업 필요

---
+++ components/TaskCard/KebabButton.tsx
@@ -0,0 +1,7 @@
+function KebabButton() {
+  return (
+    <button className="ml-auto h-pr-16 w-pr-16 flex-shrink-0 bg-[url('/images/icon_kebab.svg')] text-xs mo:ml-0" />
+  );
+}
+
+export default KebabButton;
