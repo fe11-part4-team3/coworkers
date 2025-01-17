3a11b83700334cf05de3979bc1d54862b05aca01
HwichulShin <shinhwiiron@gmail.com>
2025-01-17 21:07:08 +0900

    💄 IconLabel 컴포넌트 추가
    아이콘과 텍스트(날짜, 시간, 댓글 수 등)의 컴포넌트

---
+++ components/TaskCard/IconLabel.tsx
@@ -0,0 +1,34 @@
+interface IconTextProps {
+  text: string | number;
+  type: 'calendar' | 'time' | 'repeat' | 'commentCount';
+  hasBar?: true;
+}
+
+const IconText = ({
+  text,
+  type,
+  hasBar,
+}: IconTextProps) => {
+  const iconClass = {
+    calendar:
+      "before:bg-[url('/images/icon_calendar.svg')]",
+    time: "before:bg-[url('/images/icon_time.svg')]",
+    repeat: "before:bg-[url('/images/icon_repeat.svg')]",
+    commentCount:
+      "before:bg-[url('/images/icon_comment.svg')]",
+  }[type];
+
+  return (
+    <span
+      className={`relative flex items-center text-xs text-slate-500 before:inline-block before:h-pr-16 before:w-pr-16 before:flex-1 before:bg-no-repeat before:content-[''] ${iconClass} ${type === 'commentCount' ? 'ml-pr-12 mr-pr-8 gap-pr-2 mo:ml-auto' : 'gap-pr-6'} ${
+        hasBar
+          ? 'after:absolute after:right-[-10px] after:top-1/2 after:inline-block after:h-pr-8 after:w-pr-1 after:translate-y-[-50%] after:bg-slate-700 after:content-[""]'
+          : ''
+      }`}
+    >
+      {text}
+    </span>
+  );
+};
+
+export default IconText;
