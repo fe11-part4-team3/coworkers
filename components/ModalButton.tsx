1a5b3a98717903bf76753797fdf9441ab3ee6d53
JUYOUNG\vilio <vilioite@naver.com>
2025-01-18 06:47:09 +0900

    ♻️  모달 컴포넌트에서 사용하는 버튼 컴포넌트화

---
+++ components/ModalButton.tsx
@@ -0,0 +1,48 @@
+/**
+ * ModalButton 컴포넌트는 모달 창에서 사용할 수 있는 버튼을 렌더링합니다.
+ * 버튼의 텍스트, 클릭 이벤트, 색상 스타일을 조정할 수 있습니다.
+ *
+ * @param {string} text - 버튼에 표시될 텍스트
+ * @param {Function} onClick - 버튼 클릭 시 호출되는 함수
+ * @param {'primary' | 'primaryReverse' | 'danger' | 'dangerReverse'} color - 버튼의 색상 스타일
+ *   - 'primary': 기본 스타일 (배경색: 브랜드 색상, 텍스트: 흰색)
+ *   - 'primaryReverse': 반전 스타일 (배경색: 흰색, 텍스트: 브랜드 색상, 테두리 있음)
+ *   - 'danger': 위험 스타일 (배경색: 빨간색, 텍스트: 흰색)
+ *   - 'dangerReverse': 반전 위험 스타일 (배경색: 흰색, 텍스트: 기본 색상, 테두리 있음)
+ *
+ * @returns {JSX.Element} 렌더링된 버튼 컴포넌트
+ */
+
+export default function ModalButton({
+  text,
+  onClick,
+  color,
+}: {
+  text: string;
+  onClick: () => void;
+  color:
+    | 'primary'
+    | 'primaryReverse'
+    | 'danger'
+    | 'dangerReverse';
+}) {
+  const buttonStyle = {
+    primary: 'bg-brand-primary text-white',
+    primaryReverse:
+      'bg-white text-brand-primary border border-brand-primary',
+    danger: 'bg-s-danger text-white',
+    dangerReverse:
+      'bg-white text-t-default border border-t-default',
+  };
+
+  const style = buttonStyle[color];
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${style} font-semiBold flex h-pr-48 w-pr-280 items-center justify-center rounded-xl`}
+    >
+      {text}
+    </button>
+  );
+}
--- components/modal/InviteMember.tsx
+++ components/modal/InviteMember.tsx
@@ -1,4 +1,5 @@
 import useModalStore from '@/stores/modalStore';
+import ModalButton from '@/components/ModalButton';
 
 /**
  * 멤버 초대 모달 컴포넌트.
@@ -31,12 +32,11 @@ export default function InviteMember({
           그룹에 참여할 수 있는 링크를 복사합니다.
         </p>
       </div>
-      <button
+      <ModalButton
+        text="링크 복사하기"
         onClick={handleOnclick}
-        className="font-semiBold flex h-pr-48 w-pr-280 items-center justify-center rounded-xl bg-brand-primary text-lg text-white"
-      >
-        링크 복사하기
-      </button>
+        color="primary"
+      />
     </div>
   );
 }
