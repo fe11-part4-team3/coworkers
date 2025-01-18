fa32b317c25fe1ca781ac6736ad4ef0b93d1f0f1
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-19 01:34:48 +0900

    🐛 InputField 경로 에러 수정

--- components/InputField/InputField.tsx
+++ components/InputField/InputField.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { Input } from '../ui/input';
-import VisibilityToggle from './visibilityToggle';
+import VisibilityToggle from './VisibilityToggle';
 
 export interface InputFieldProps {
   value: string;
