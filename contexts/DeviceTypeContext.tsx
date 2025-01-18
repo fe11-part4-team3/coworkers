2b2cd228fb14e2b367daffa3fd2c4924bef4a5b3
WooLegend <dnwogus4260@naver.com>
2025-01-19 02:29:11 +0900

    🐛 window undefined일 때 예외처리

--- contexts/DeviceTypeContext.tsx
+++ contexts/DeviceTypeContext.tsx
@@ -12,6 +12,7 @@ export type DeviceType = 'desktop' | 'tablet' | 'mobile';
 const DeviceTypeContext = createContext<DeviceType>('desktop');
 
 const getDeviceType = (): DeviceType => {
+  if (typeof window === 'undefined') return 'desktop';
   if (window.innerWidth < 768) return 'mobile';
   if (window.innerWidth < 1280) return 'tablet';
   return 'desktop';
@@ -26,12 +27,15 @@ export function DeviceTypeProvider({
 
   const handleResize = useCallback(
     useDebounce(() => {
-      setDeviceType(getDeviceType());
+      if (typeof window === 'undefined') {
+        setDeviceType(getDeviceType());
+      }
     }, 100),
     [],
   );
 
   useEffect(() => {
+    if (typeof window === 'undefined') return;
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, [handleResize]);
