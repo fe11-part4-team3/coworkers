27b356af6e379b2b551f96cdd3ef1369e193a16f
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-19 00:26:02 +0900

    ✨ TextareaField 컴포넌트 제작

---
+++ components/InputField/TextareaField.tsx
@@ -0,0 +1,48 @@
+import { inputStyled } from './InputField';
+import { Textarea } from '../ui/textarea';
+import { TextareaFieldProps } from '@/types/inputField.type';
+import InputLabel from './InputLabel';
+
+const sizes = {
+  md: 'py-pr-12 px-pr-16 h-pr-75',
+  lg: 'py-pr-16 px-pr-24 mo:py-pr-8 mo:px-pr-16 h-pr-104',
+};
+
+/**
+ * @description TextareaField 컴포넌트
+ * @param {string} props.size - textarea 크기
+ * @property {'md' | 'lg'} [size='md'] - (md - 'py-pr-12 px-pr-16', lg - 'py-pr-16 px-pr-24')
+ * @param {string} props.value - textarea 값
+ * @param {string} props.height - textarea 높이
+ * @param {string} props.label - textarea 라벨
+ * @param {boolean} props.essential - 필수 여부
+ * @param {string} props.placeholder - textarea placeholder
+ * @param {boolean} props.disabled - textarea 비활성화 여부
+ * @param {Function} props.onChange - textarea 값 변경 이벤트
+ *
+ */
+export default function TextareaField({
+  size = 'md',
+  value,
+  height = '',
+  label = '',
+  essential = false,
+  placeholder = 'placeholder를 작성해주세요',
+  disabled = false,
+  onChange,
+  ...props
+}: TextareaFieldProps) {
+  return (
+    <fieldset>
+      {label && <InputLabel label={label} essential={essential} />}
+      <Textarea
+        className={`${inputStyled} ${sizes[size]} ${height} resize-none`}
+        value={value}
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={onChange}
+        {...props}
+      />
+    </fieldset>
+  );
+}
