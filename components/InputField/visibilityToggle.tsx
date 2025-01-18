b2ee01b8fefc73715e4013a1c967273933695174
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-18 15:10:46 +0900

    ✨ password input 일 경우 사용될 토글 아이콘 컴포넌트

---
+++ components/InputField/visibilityToggle.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+interface VisibilityToggleProps {
+  togglePassword: () => void;
+  showPassword: boolean;
+}
+
+/**
+ * @description 비밀번호 보이기/숨기기 버튼
+ * @param {Function} props.togglePassword - 비밀번호 보이기/숨기기 토글 함수
+ * @param {boolean} props.showPassword - 비밀번호 보이기 여부
+ */
+export default function VisibilityToggle({
+  togglePassword,
+  showPassword,
+}: VisibilityToggleProps) {
+  return (
+    <button
+      type="button"
+      onClick={togglePassword}
+      className="absolute right-pr-16 top-1/2 -translate-y-1/2 transform"
+    >
+      <Image
+        src={
+          showPassword
+            ? 'images/icon-visibility_on.svg'
+            : 'images/icon-visibility_off.svg'
+        }
+        alt="visibility_off"
+        width={24}
+        height={24}
+      />
+    </button>
+  );
+}
