2250029f3e99547b9f687417e514f3c7998d2f0d
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-18 15:09:54 +0900

    💄 tailwind -ring 컬러 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,7 +47,7 @@ const shadcnConfig: Config = {
           foreground: 'hsl(var(--destructive-foreground))',
         },
         input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
+        ring: 'var(--ring)',
         chart: {
           '1': 'hsl(var(--chart-1))',
           '2': 'hsl(var(--chart-2))',
