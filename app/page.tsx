5e2739524839d9343ec9373a9fa18621705a6fee
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-19 05:41:00 +0900

    🔨 랜딩페이지 컨텐츠 원복

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Container from '@/components/layout/Container';
-import Header from '@/components/NavBar/Header';
+import Header from '@/components/layout/Header/Headers';
 export default function LandingPage() {
   return (
     <Container>
-      <div>
-        <Header />
-      </div>
+      <h1>메인 페이지</h1>
     </Container>
   );
 }
