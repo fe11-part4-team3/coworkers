93454407ed8c2f55f1caea2a651903d0bb5c9046
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-19 17:07:32 +0900

    🔨 모든 페이지에 유저정보 초기화를 위해 initializeUserData 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,14 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './globals.css';
 import './globals-custom.css';
 
+import { useEffect } from 'react';
+
 import { ThemeProvider } from '@/utils/theme-provider';
 import DarkmodeToggle from '@/components/DarkmodeToggle';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import Headers from '@/components/layout/Header/Headers';
 import { DeviceTypeProvider } from '@/contexts/DeviceTypeContext';
+import useUserStore from '@/store/useUser.store';
 
 const queryClient = new QueryClient();
 export default function RootLayout({
@@ -18,6 +21,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const initializeUserData = useUserStore((state) => state.initializeUserData);
+
+  useEffect(() => {
+    initializeUserData(); // Zustand 상태 초기화
+  }, [initializeUserData]);
+
   return (
     <html lang="ko">
       <body>
