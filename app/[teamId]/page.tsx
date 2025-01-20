e2e7efa687c20f5127504204322cc73880e18348
WooLegend <dnwogus4260@naver.com>
2025-01-20 18:37:53 +0900

    🔨  groups 데이터 패칭

--- app/[teamId]/page.tsx
+++ app/[teamId]/page.tsx
@@ -1,39 +1,48 @@
 'use client';
 
 import Container from '@/components/layout/Container';
-import getMockGroups from '@/components/SideNavigation/mockGroups';
-import SideNavigationBar from '@/components/SideNavigation/SideNavigation';
+import SideNavigation from '@/components/SideNavigation/SideNavigation';
 import SideNavigationTrigger from '@/components/SideNavigation/SideNavigationTrigger';
-import GroupMemberCard from '@/components/GroupMemberCard.tsx/GroupMemberCard';
-import { IMember } from '@/types/group.type';
+import { useAuth } from '@/hooks/useAuth';
+import { getGroupList } from '@/service/user.api';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
-const MOCK_MEMBER: IMember = {
-  role: 'ADMIN',
-  userImage: null,
-  userEmail: 'example@example.com',
-  userName: '김쁠뿡',
-  groupId: 0,
-  userId: 0,
-};
+// import { IMember } from '@/types/group.type';
+// const MOCK_MEMBER: IMember = {
+//   role: 'ADMIN',
+//   userImage: null,
+//   userEmail: 'example@example.com',
+//   userName: '김쁠뿡',
+//   groupId: 0,
+//   userId: 0,
+// };
 
 export default function TeamPage() {
-  const groups = getMockGroups(10);
+  const router = useRouter();
+  const { isAuthenticated } = useAuth();
+  const { data: groups } = useQuery({
+    queryKey: ['groups'],
+    queryFn: getGroupList,
+  });
+
+  if (!isAuthenticated) {
+    router.push('/');
+    return null;
+  }
 
   return (
-    <>
-      <SideNavigationBar
+    <Container>
+      <SideNavigation
         groups={groups}
         loading={false}
         showSkeleton={true}
         skeletonLength={10}
       />
-      <Container>
-        <SideNavigationTrigger
-          src="images/icon-gnb-menu.svg"
-          alt="사이드 네비게이션 열기"
-        />
-        <GroupMemberCard member={MOCK_MEMBER} />
-      </Container>
-    </>
+      <SideNavigationTrigger
+        src="images/icon-gnb-menu.svg"
+        alt="사이드 네비게이션 열기"
+      />
+    </Container>
   );
 }
