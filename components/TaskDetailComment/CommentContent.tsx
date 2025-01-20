678e212396f088b8f8772891884d57dc4940112f
HwichulShin <shinhwiiron@gmail.com>
2025-01-20 22:04:33 +0900

    💄 kebab css 수정

--- components/TaskDetailComment/CommentContent.tsx
+++ components/TaskDetailComment/CommentContent.tsx
@@ -3,7 +3,7 @@
  * @returns {JSX.Element} 댓글 내용 컴포넌트
  */
 function CommentContent({ content }: { content: string }) {
-  return <p className="break-all pr-pr-16 text-14 text-t-primary">{content}</p>;
+  return <p className="break-all text-14 text-t-primary">{content}</p>;
 }
 
 export default CommentContent;
