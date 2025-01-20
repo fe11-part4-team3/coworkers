fd258c6ba285bf42a437832ddc13af86420d324f
HwichulShin <shinhwiiron@gmail.com>
2025-01-20 12:47:09 +0900

    💄 자유게시판 검색 Input, 돋보기 아이콘 추가

---
+++ components/SearchInput/SearchInput.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent } from 'react';
+import { inputStyled } from '../InputField/InputField';
+import { Input } from '../ui/input';
+
+interface SearchInputProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function SearchInput({ value, onChange }: SearchInputProps) {
+  return (
+    <div className="relative">
+      <button
+        type="submit"
+        className="absolute left-pr-16 top-1/2 size-pr-24 -translate-y-1/2 bg-[url('/images/icon-search.svg')] content-['']"
+      />
+      <Input
+        type="text"
+        className={`${inputStyled} h-pr-56 pl-pr-52 pr-pr-16 hover:border-i-hover mo:h-pr-48`}
+        placeholder="검색어를 입력해주세요"
+        name="keyword"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default SearchInput;
