aad6fd2495a91bd280760bc6592aeec548e1289a
HwichulShin <shinhwiiron@gmail.com>
2025-01-20 09:31:35 +0900

    💄 할 일 카드 Dropdown 추가

--- components/TaskCard/KebabButton.tsx
+++ components/TaskCard/KebabButton.tsx
@@ -1,6 +1,25 @@
-function KebabButton() {
+import DropDown from '../DropDown';
+
+function KebabButton({ taskId }: { taskId: number }) {
   return (
-    <button className="ml-auto size-pr-16 shrink-0 bg-[url('/images/icon-kebab.svg')] text-xs mo:ml-0" />
+    <div className="ml-auto size-pr-16 mo:ml-0">
+      <DropDown
+        trigger={
+          <button className="size-pr-16 shrink-0 bg-[url('/images/icon-kebab.svg')] text-xs" />
+        }
+        items={[
+          {
+            text: '수정하기',
+            onClick: () => alert(`${taskId} 할 일 카드 수정하기`),
+          },
+          {
+            text: '삭제하기',
+            onClick: () => alert(`${taskId} 할 일 카드 삭제하기`),
+          },
+        ]}
+        width="w-pr-120"
+      />
+    </div>
   );
 }
 
