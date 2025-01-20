218f881f50f594e080c00dbb6ce22d0fa0fc7924
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-20 14:34:51 +0900

    🧪 테스트를 위한 팀생성 페이지 임의 추가

--- app/addteam/page.tsx
+++ app/addteam/page.tsx
@@ -1,3 +1,31 @@
+'use client';
+
+import InputField from '@/components/InputField/InputField';
+import Container from '@/components/layout/Container';
+import { Button } from '@/components/ui/button';
+import useForm from '@/hooks/useForm';
+
 export default function AddTeamPage() {
-  return <div>팀 생성 페이지</div>;
+  const { formData, handleChange } = useForm({ name: '' });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Container>
+      <h1>팀생성</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" />
+        <InputField
+          value={formData.name}
+          onChange={handleChange}
+          name="name"
+          placeholder="팀 이름을 입력해주세요."
+        />
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Container>
+  );
 }
