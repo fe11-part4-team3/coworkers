62c61ac90a6dbdb901ab8ceb0942542bdb401e0c
HwichulShin <shinhwiiron@gmail.com>
2025-01-20 16:10:47 +0900

    💄 프로필 컴포넌트 추가

---
+++ components/TaskDetailComment/Profile.tsx
@@ -0,0 +1,27 @@
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
+
+interface ProfileProp {
+  user: {
+    id: number;
+    nickname: string;
+    image: string | null;
+  };
+}
+
+const DEFAULT_PROFILE = '/images/icon-profile-member-default.svg';
+
+function Profile({ user }: ProfileProp) {
+  const { nickname, image } = user;
+
+  return (
+    <div className="flex items-center">
+      <Avatar className="size-pr-32">
+        <AvatarImage src={image || DEFAULT_PROFILE} className="rounded-full" />
+      </Avatar>
+
+      <span className="ml-pr-12 text-14m text-t-primary">{nickname}</span>
+    </div>
+  );
+}
+
+export default Profile;
