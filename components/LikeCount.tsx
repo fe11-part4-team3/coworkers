0cc950a5069f66096f9f6b2b61976a0bf6522e5e
HwichulShin <shinhwiiron@gmail.com>
2025-01-21 14:39:39 +0900

    💄 좋아요 카운드 추가

---
+++ components/LikeCount.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function LikeCount({
+  type,
+  likeCount,
+}: {
+  type: 'readOnly' | 'interactive';
+  likeCount: number;
+}) {
+  const [isCheck, setIsCheck] = useState(false);
+  const [currentCount, setCurrentCount] = useState(likeCount);
+
+  const handleClick = () => {
+    setIsCheck(!isCheck);
+    setCurrentCount(isCheck ? currentCount - 1 : currentCount + 1);
+  };
+
+  return (
+    <>
+      <button
+        className="text-14 text-t-disabled"
+        onClick={handleClick}
+        disabled={type === 'readOnly'}
+      >
+        {!isCheck ? '♡' : '♥︎'}
+        <span className="ml-pr-4">
+          {currentCount >= 9999 ? '9999+' : currentCount}
+        </span>
+      </button>
+    </>
+  );
+}
+
+export default LikeCount;
