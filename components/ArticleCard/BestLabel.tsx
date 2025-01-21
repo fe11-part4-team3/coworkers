57f529ec296ade260ab9cf6276db85d8fc46b7d8
HwichulShin <shinhwiiron@gmail.com>
2025-01-21 14:39:17 +0900

    💄 Best 라벨 추가

---
+++ components/ArticleCard/BestLabel.tsx
@@ -0,0 +1,12 @@
+import BestMedal from '@/public/images/icon-medal.svg';
+
+function BestLabel() {
+  return (
+    <div className="flex items-center">
+      <BestMedal />
+      <span className="ml-pr-4">Best</span>
+    </div>
+  );
+}
+
+export default BestLabel;
