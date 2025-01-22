97dfa73481d91901702a386b15185a2282587e7f
HwichulShin <shinhwiiron@gmail.com>
2025-01-22 18:49:50 +0900

    🚚 작성자 프로필 재사용 가능하도록 파일위치 변경 및 수정

---
+++ components/WriterProfile.tsx
@@ -0,0 +1,26 @@
+import Profile from '@/components/Profile/Profile';
+
+/**
+ * @param {number} props.id - 게시글 작성자 유저 id
+ * @param {string} props.nickname - 게시글 작성자 유저 닉네임
+ * @returns {JSX.Element} 게시글 카드의 작성자 프로필 컴포넌트
+ */
+function WriterProfile({
+  writer,
+}: {
+  writer: {
+    id: number;
+    nickname: string;
+  };
+}) {
+  const { nickname } = writer;
+
+  return (
+    <div className="flex items-center">
+      <Profile variant="member" profileSize={32} />
+      <span className="ml-pr-12 text-14m mo:text-12m">{nickname}</span>
+    </div>
+  );
+}
+
+export default WriterProfile;
