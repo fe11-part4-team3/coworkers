8bd21f4390e98618c28c475b85233a938eb8357e
JUYOUNG\vilio <vilioite@naver.com>
2025-01-23 08:27:45 +0900

    🚨 timepicker 스토리북 제작 및 버그 수정

--- components/DateTimePicker/TimePicker.tsx
+++ components/DateTimePicker/TimePicker.tsx
@@ -26,54 +26,57 @@ export default function TimePicker({ width = 'full' }: { width?: string }) {
 
   return (
     <>
-      <div className="relative">
-        <InputField
-          value={time ? (amPm === 'am' ? `오전 ${time}` : `오후 ${time}`) : ''}
-          placeholder="시간을 선택해주세요."
-          disabled={true}
-          onChange={() => {}}
-          name="date"
-          width={`${width !== 'full' ? `w-pr-${width}` : ''}`}
-        />
-        <button
-          className={`z-90 absolute left-0 top-0 size-full rounded-xl ${isOpen ? 'border border-brand-primary' : ''}`}
-          onClick={() => setIsOpen(!isOpen)}
-        />
-      </div>
-      {isOpen && (
-        <div
-          className={`${width !== 'full' ? `w-pr-${width}` : 'w-full'} flex gap-pr-14 rounded-xl border border-brand-primary bg-b-secondary p-pr-12`}
-        >
-          <div className="flex flex-col gap-pr-8">
-            <button
-              onClick={() => setAmPm('am')}
-              className={`h-pr-40 w-pr-78 rounded-xl bg-b-primary text-14m text-t-default ${amPm === 'am' ? 'bg-brand-primary text-white' : ''}`}
-            >
-              오전
-            </button>
-            <button
-              onClick={() => setAmPm('pm')}
-              className={`h-pr-40 w-pr-78 rounded-xl bg-b-primary text-14m text-t-default ${amPm === 'pm' ? 'bg-brand-primary text-white' : ''}`}
-            >
-              오후
-            </button>
-          </div>
-          <ol className="scrollbar flex h-pr-152 w-full list-none flex-col gap-pr-16 overflow-y-auto rounded-xl bg-b-primary p-pr-16 text-16 text-t-default">
-            {timeSlots().map((timeSlot, index) => {
-              const isSelected = time === timeSlot;
-              return (
-                <button
-                  key={index}
-                  onClick={() => setTime(timeSlot)}
-                  className={`text-left ${isSelected ? 'text-brand-primary' : ''}`}
-                >
-                  <li>{timeSlot}</li>
-                </button>
-              );
-            })}
-          </ol>
+      <div
+        className={`flex flex-col gap-pr-8 ${width !== 'full' ? `w-pr-${width}` : ''}`}
+      >
+        <div className="relative">
+          <InputField
+            value={
+              time ? (amPm === 'am' ? `오전 ${time}` : `오후 ${time}`) : ''
+            }
+            placeholder="시간을 선택해주세요."
+            disabled={true}
+            onChange={() => {}}
+            name="date"
+          />
+          <button
+            className={`z-90 absolute left-0 top-0 size-full rounded-xl ${isOpen ? 'border border-brand-primary' : ''}`}
+            onClick={() => setIsOpen(!isOpen)}
+          />
         </div>
-      )}
+        {isOpen && (
+          <div className="flex gap-pr-14 rounded-xl border border-brand-primary bg-b-secondary p-pr-12">
+            <div className="flex flex-col gap-pr-8">
+              <button
+                onClick={() => setAmPm('am')}
+                className={`h-pr-40 w-pr-78 rounded-xl bg-b-primary text-14m text-t-default ${amPm === 'am' ? 'bg-brand-primary text-white' : ''}`}
+              >
+                오전
+              </button>
+              <button
+                onClick={() => setAmPm('pm')}
+                className={`h-pr-40 w-pr-78 rounded-xl bg-b-primary text-14m text-t-default ${amPm === 'pm' ? 'bg-brand-primary text-white' : ''}`}
+              >
+                오후
+              </button>
+            </div>
+            <ol className="scrollbar flex h-pr-152 w-full list-none flex-col gap-pr-16 overflow-y-auto rounded-xl bg-b-primary p-pr-16 text-16 text-t-default">
+              {timeSlots().map((timeSlot, index) => {
+                const isSelected = time === timeSlot;
+                return (
+                  <button
+                    key={index}
+                    onClick={() => setTime(timeSlot)}
+                    className={`text-left ${isSelected ? 'text-brand-primary' : ''}`}
+                  >
+                    <li>{timeSlot}</li>
+                  </button>
+                );
+              })}
+            </ol>
+          </div>
+        )}
+      </div>
     </>
   );
 }
