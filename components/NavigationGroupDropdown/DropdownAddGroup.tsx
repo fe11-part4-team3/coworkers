6c212f0aef5e5abb9206b3895812a560b59b502e
WooLegend <dnwogus4260@naver.com>
2025-01-24 06:22:35 +0900

    🎨 icon-plus 색상 currentColor로 svg 수정

--- components/NavigationGroupDropdown/DropdownAddGroup.tsx
+++ components/NavigationGroupDropdown/DropdownAddGroup.tsx
@@ -1,6 +1,6 @@
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import Plus from '@/public/images/icon-plus.svg';
+import IconPlus from '@/public/images/icon-plus.svg';
 
 /**
  * 그룹 추가 버튼
@@ -10,7 +10,7 @@ export default function DropdownAddGroup() {
   return (
     <DropdownMenuItem asChild>
       <Button className="mt-pr-16 w-full rounded-lg border border-slate-50 bg-inherit">
-        <Plus />
+        <IconPlus className="text-t-primary" />
         <span className="text-t-primary">팀 추가하기</span>
       </Button>
     </DropdownMenuItem>
