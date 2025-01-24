b7f25323bcd08f6ed2648510d0f1cb9c79f82928
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-24 23:43:31 +0900

    ✨ useForm 유효성 검사 로직 추가

--- hooks/useForm.ts
+++ hooks/useForm.ts
@@ -15,18 +15,77 @@ import { useState } from 'react';
  */
 const useForm = <T extends Record<string, any>>(initialValues: T) => {
   const [formData, setFormData] = useState<T>(initialValues);
+  const [errorMessage, setErrorMessage] = useState<
+    Partial<Record<keyof T, string>>
+  >({});
+
+  const rPASSWORD =
+    /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+
+  const validateField = (name: keyof T, value: string) => {
+    const errors: Partial<Record<keyof T, string>> = {};
+
+    switch (name) {
+      case 'email':
+        if (!value.includes('@')) {
+          errors[name] = '이메일 형식으로 작성해 주세요';
+        } else if (value.length === 0) {
+          errors[name] = '이메일은 필수 입력입니다.';
+        } else {
+          errors[name] = '';
+        }
+        break;
+      case 'password':
+        if (value.length < 8) {
+          errors[name] = '비밀번호는 최소 8자 이상입니다.';
+        } else if (!rPASSWORD.test(value)) {
+          errors[name] = '비밀번호는 숫자, 영문, 특수문자로만 가능합니다.';
+        } else if (value.length === 0) {
+          errors[name] = '비밀번호는 필수 입력입니다.';
+        } else {
+          errors[name] = '';
+        }
+        break;
+      case 'passwordConfirmation':
+        if (value !== formData.password) {
+          errors[name] = '비밀번호가 일치하지 않습니다.';
+        } else if (value.length === 0) {
+          errors[name] = '비밀번호 확인을 입력해주세요.';
+        } else {
+          errors[name] = '';
+        }
+        break;
+      case 'nickname':
+        if (value.length > 20) {
+          errors[name] = '닉네임은 최대 20자까지 가능합니다.';
+        } else if (value.length === 0) {
+          errors[name] = '닉네임은 필수 입력입니다.';
+        } else {
+          errors[name] = '';
+        }
+        break;
+      default:
+        break;
+    }
+
+    return errors;
+  };
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
+
+    const fieldErrors = validateField(name as keyof T, value);
+    setErrorMessage((prev) => ({ ...prev, ...fieldErrors }));
   };
 
   return {
     formData,
     handleChange,
     setFormData,
+    errorMessage,
   };
 };
 
