24eadf1fe1710d75957637750980d88021dec35e
tare <59001439+junghwa1996@users.noreply.github.com>
2025-01-25 19:07:56 +0900

    💄 모달베이스 모바일 위치 추가

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -9,9 +9,9 @@ export default function Modal() {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 mo:items-end">
       <div
-        className="relative flex w-pr-384 flex-col items-center justify-between rounded-xl bg-popover px-pr-30 pb-pr-32 pt-pr-40 mo:bottom-0 mo:w-full mo:rounded-b-none"
+        className="relative flex w-pr-384 flex-col items-center justify-between rounded-xl bg-popover px-pr-30 pb-pr-32 pt-pr-40 mo:w-full mo:rounded-b-none"
         onClick={handleContentClick}
       >
         <div className="w-full">{modalContent}</div>
