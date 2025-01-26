63e164c68cb8328506940bb212cf69fdb6345740
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-01-26 15:37:34 +0900

    🔨 팀 페이지 그룹 헤더 드롭 다운 구현

--- app/[teamId]/(index)/GroupHeader.tsx
+++ app/[teamId]/(index)/GroupHeader.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 
 import IconGear from '@/public/images/icon-gear.svg';
+import DropDown from '@/components/DropDown';
 
 interface GroupHeaderProps {
   name: string;
@@ -19,16 +20,22 @@ export default function GroupHeader({
   return (
     <header className="flex max-h-pr-64 max-w-pr-1200 items-center justify-between rounded-pr-12 border border-[#F8FAFC1A] bg-[--t-primary-dark-10] px-pr-20">
       <h2 className="text-20b text-primary">{name}</h2>
-      <div className="flex gap-pr-20">
+      <div className="flex items-center gap-pr-20">
         <Image
           src="/images/img-thumbnail-team.png"
           alt=""
           width={181}
           height={64}
         />
-        <button onClick={() => {}}>
-          <IconGear className="transition-all hover:rotate-90 hover:scale-110" />
-        </button>
+        <DropDown
+          trigger={
+            <IconGear className="transition-all hover:rotate-90 hover:scale-110 focus:rotate-90 data-[state=open]:rotate-90 data-[state=open]:scale-110" />
+          }
+          items={[
+            { text: '수정하기', onClick: () => {} },
+            { text: '삭제하기', onClick: () => {} },
+          ]}
+        />
       </div>
     </header>
   );
