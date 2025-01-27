481eb2a8a91d778195cd1c22f9ebe58cadbe4eaa
WooLegend <dnwogus4260@naver.com>
2025-01-28 04:18:38 +0900

    🔨 useTaskLists 로직 개선 및 할 일 목록 제거 함수 구현

--- hooks/useTaskLists.ts
+++ hooks/useTaskLists.ts
@@ -1,9 +1,10 @@
 import { getTaskList } from '@/service/taskList.api';
-import { ITaskListSummary } from '@/types/group.type';
+import useGroupStore from '@/stores/useGroup.stroe';
 import { useQueries, useQueryClient } from '@tanstack/react-query';
 
-export default function useTaskLists(taskLists: ITaskListSummary[] | null) {
+export default function useTaskLists() {
   const queryClient = useQueryClient();
+  const { taskLists, setTaskLists } = useGroupStore();
   const queries = useQueries({
     queries:
       taskLists?.map(({ id, groupId }) => ({
@@ -15,21 +16,31 @@ export default function useTaskLists(taskLists: ITaskListSummary[] | null) {
   const data = queries
     .map((query) => query.data)
     .filter((el) => el !== undefined);
+
   const isPending = queries.some((query) => query.isPending);
+
   const error = queries.find((query) => query.error)?.error;
 
   const refetchById = async (id: number) =>
     await queryClient.refetchQueries({ queryKey: ['taskList', id] });
 
+  const removeById = (id: number) => {
+    const next = taskLists?.filter((taskList) => taskList.id !== id);
+    if (next) {
+      setTaskLists(next.length !== 0 ? next : null);
+      queryClient.removeQueries({ queryKey: ['taskList', id] });
+    }
+  };
+
   const refetchAll = async () =>
     await queryClient.refetchQueries({ queryKey: ['taskList'] });
 
   return {
-    queries,
     data,
     isPending: !!taskLists && isPending,
     error,
     refetchById,
+    removeById,
     refetchAll,
   };
 }
