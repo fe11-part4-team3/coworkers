a051afc3a57bcf126fb3b8a66c9e14c08f4642aa
WooLegend <dnwogus4260@naver.com>
2025-01-28 02:55:03 +0900

    🔨 useTaskLists에 파싱 데이터 및 유틸리티 추가

--- hooks/useTaskLists.ts
+++ hooks/useTaskLists.ts
@@ -1,19 +1,35 @@
 import { getTaskList } from '@/service/taskList.api';
 import { ITaskListSummary } from '@/types/group.type';
-import { useQueries } from '@tanstack/react-query';
+import { useQueries, useQueryClient } from '@tanstack/react-query';
 
 export default function useTaskLists(taskLists: ITaskListSummary[] | null) {
-  const data = useQueries({
-    queries: getQueries(taskLists),
+  const queryClient = useQueryClient();
+  const queries = useQueries({
+    queries:
+      taskLists?.map(({ id, groupId }) => ({
+        queryKey: ['taskList', id],
+        queryFn: () => getTaskList({ groupId, id }),
+      })) || [],
   });
 
-  return { data };
-}
+  const data = queries
+    .map((query) => query.data)
+    .filter((el) => el !== undefined);
+  const isPending = queries.some((query) => query.isPending);
+  const error = queries.find((query) => query.error)?.error;
 
-const getQueries = (taskLists: ITaskListSummary[] | null) => {
-  if (!taskLists) return [];
-  return taskLists.map(({ id, groupId }) => ({
-    queryKey: ['taskList', id],
-    queryFn: () => getTaskList({ groupId, id }),
-  }));
-};
+  const refetchById = async (id: number) =>
+    await queryClient.refetchQueries({ queryKey: ['taskList', id] });
+
+  const refetchAll = async () =>
+    await queryClient.refetchQueries({ queryKey: ['taskList'] });
+
+  return {
+    queries,
+    data,
+    isPending: !!taskLists && isPending,
+    error,
+    refetchById,
+    refetchAll,
+  };
+}
