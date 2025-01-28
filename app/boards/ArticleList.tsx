900efd4df81dea3cb4099364a4a2974ce9445976
HwichulShin <shinhwiiron@gmail.com>
2025-01-29 00:09:16 +0900

    🐛 무한스크롤 기능 임시 제거(추후 재작업 예정)

--- app/boards/ArticleList.tsx
+++ app/boards/ArticleList.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState } from 'react';
 
 import ArticleCard from '@/components/ArticleCard/ArticleCard';
 import SelectBox from '@/components/SelectBox';
 import useGetArticle from '@/hooks/useGetArticle';
-import { IArticle } from '@/types/article.type';
 
 import ArticleSkeleton from './ArticleSkeleton';
 import EmptyList from './EmptyList';
@@ -16,9 +15,6 @@ import EmptyList from './EmptyList';
  */
 function ArticleList({ keyword }: { keyword: string | undefined }) {
   const [option, setOption] = useState<'recent' | 'like'>('recent');
-  const [page, setPage] = useState(1);
-  const [articles, setArticles] = useState<IArticle[]>([]);
-  const [hasMore, setHasMore] = useState(true); // 더 불러올 데이터가 있는지 여부
 
   const {
     data: articleList,
@@ -26,52 +22,16 @@ function ArticleList({ keyword }: { keyword: string | undefined }) {
     isError,
   } = useGetArticle({
     queryKey: 'bestArticleList',
-    pageSize: 10,
+    pageSize: 100,
     orderBy: option,
     keyword: keyword ?? '',
-    page,
   });
 
-  useEffect(() => {
-    if (articleList?.list) {
-      setArticles((prevArticles) => [...prevArticles, ...articleList.list]);
-
-      // 불러온 데이터 수가 totalCount에 도달하면 더 이상 로드하지 않음
-      if (articles.length + articleList.list.length >= articleList.totalCount) {
-        setHasMore(false);
-      }
-    }
-  }, [articleList]);
-
-  const handleScroll = useCallback(() => {
-    if (
-      !hasMore ||
-      window.innerHeight + document.documentElement.scrollTop <
-        document.documentElement.offsetHeight ||
-      isLoading
-    ) {
-      return; // 더 불러올 데이터가 없으면 리턴
-    }
-    setPage((prevPage) => prevPage + 1);
-  }, [isLoading, hasMore]);
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [handleScroll]);
-
-  useEffect(() => {
-    // 옵션 변경 시 페이지 및 리스트 초기화
-    setPage(1);
-    setArticles([]);
-    setHasMore(true); // 옵션이 변경되면 데이터 다시 로드 가능하도록 초기화
-  }, [option, keyword]);
-
-  if (isError) return <div>에러가 발생했습니다.</div>;
+  if (isError) return '에러가 발생했습니다.';
 
   return (
     <>
-      {articles.length > 0 ? (
+      {!(articleList?.list.length === 0) ? (
         <section className="mt-pr-40">
           <div className="flex justify-between">
             <h3 className="text-20b">게시글</h3>
@@ -88,14 +48,19 @@ function ArticleList({ keyword }: { keyword: string | undefined }) {
             />
           </div>
           <div className="mt-pr-32 flex flex-wrap justify-between gap-y-pr-24">
-            {articles.map((article) => (
-              <ArticleCard key={article.id} articleData={article} />
-            ))}
-            {isLoading && <ArticleSkeleton count={4} />}
+            {!isLoading ? (
+              <>
+                {articleList?.list.map((article) => {
+                  return <ArticleCard key={article.id} articleData={article} />;
+                })}
+              </>
+            ) : (
+              <ArticleSkeleton count={4} />
+            )}
           </div>
         </section>
       ) : (
-        !isLoading && <EmptyList />
+        <EmptyList />
       )}
     </>
   );
--- hooks/useGetArticle.ts
+++ hooks/useGetArticle.ts
@@ -5,7 +5,6 @@ import { getArticleList } from '@/service/article.api';
 interface GetArticleProps {
   queryKey: string;
   pageSize: number;
-  page?: number;
   orderBy?: 'recent' | 'like';
   keyword?: string;
   deviceType?: 'desktop' | 'tablet' | 'mobile';
@@ -14,17 +13,15 @@ interface GetArticleProps {
 const useGetArticle = ({
   queryKey,
   pageSize,
-  page,
   orderBy = 'recent',
   keyword = '',
   deviceType,
 }: GetArticleProps) => {
   const { data, isLoading, isError } = useQuery({
-    queryKey: [{ queryKey }, page, orderBy, deviceType, keyword],
+    queryKey: [{ queryKey }, orderBy, deviceType, keyword],
     queryFn: () =>
       getArticleList({
         pageSize: pageSize,
-        page: page,
         orderBy: orderBy,
         keyword: keyword,
       }),
