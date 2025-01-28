d6ba04190ee0e38baaa10d98df419d11ff2dbee6
HwichulShin <shinhwiiron@gmail.com>
2025-01-29 00:41:02 +0900

    💄 조회된 게시글 없는 경우 스타일 수정

--- app/boards/EmptyList.tsx
+++ app/boards/EmptyList.tsx
@@ -5,9 +5,14 @@ import Image from 'next/image';
  */
 function EmptyList() {
   return (
-    <div className="mx-auto mt-pr-180 w-pr-810">
-      <div className="relative h-pr-255">
-        <Image src="/images/img-noTeam.png" alt="" fill />
+    <div className="mx-auto mt-pr-180 w-pr-810 tamo:w-auto">
+      <div className="relative h-pr-255 mo:h-pr-100 ta:h-pr-180">
+        <Image
+          src="/images/img-noTeam.png"
+          alt=""
+          fill
+          className="object-contain"
+        />
       </div>
       <p className="mt-pr-48 text-center text-16m text-t-default">
         조회된 게시글이 없습니다.
