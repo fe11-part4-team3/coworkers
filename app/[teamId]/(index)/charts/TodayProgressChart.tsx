55b8218dd29cb54d3cf5159608ff2f97816a3cf9
WooLegend <dnwogus4260@naver.com>
2025-02-01 04:48:04 +0900

    🔨 오늘의 진행 상황 차트 컴포넌트 구현

---
+++ app/[teamId]/(index)/charts/TodayProgressChart.tsx
@@ -0,0 +1,102 @@
+import { Label, PolarRadiusAxis, RadialBar, RadialBarChart } from 'recharts';
+
+import { ITask } from '@/types/task.type';
+import parseTasks from '@/utils/parseTasks';
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
+
+const COLOR = {
+  done: 'var(--brand-primary)',
+  todo: 'var(--s-danger)',
+};
+
+const chartConfig = {
+  done: {
+    label: 'Done',
+    color: 'hsl(var(--brand-primary))',
+  },
+  todo: {
+    label: 'Todo',
+    color: 'hsl(var(--s-danger))',
+  },
+} satisfies ChartConfig;
+
+interface TodayProgressChartProps {
+  tasks: ITask[];
+}
+
+export default function TodayProgressChart({ tasks }: TodayProgressChartProps) {
+  const { length, done, todo } = parseTasks(tasks);
+
+  const chartData = [{ done: done.length, todo: todo.length }];
+  const progress = length ? Math.round((done.length / length) * 100) : 100;
+
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square max-h-[250px]"
+    >
+      <RadialBarChart
+        data={chartData}
+        startAngle={0}
+        endAngle={360}
+        innerRadius={80}
+        outerRadius={130}
+      >
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+                return (
+                  <text
+                    x={viewBox.cx}
+                    y={viewBox.cy}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                  >
+                    <tspan
+                      x={viewBox.cx}
+                      y={viewBox.cy}
+                      className="fill-foreground text-4xl font-bold"
+                    >
+                      {`${progress}%`}
+                    </tspan>
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 24}
+                      className="fill-muted-foreground"
+                    >
+                      오늘의 진행 상황
+                    </tspan>
+                  </text>
+                );
+              }
+            }}
+          />
+        </PolarRadiusAxis>
+        <RadialBar
+          dataKey="done"
+          stackId="a"
+          cornerRadius={5}
+          fill={COLOR.done}
+          className="stroke-transparent stroke-2"
+        />
+        <RadialBar
+          dataKey="todo"
+          fill={COLOR.todo}
+          stackId="a"
+          cornerRadius={5}
+          className="stroke-transparent stroke-2"
+        />
+      </RadialBarChart>
+    </ChartContainer>
+  );
+}
