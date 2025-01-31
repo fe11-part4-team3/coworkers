dc20712dc8089dffd4adbc2d2ba1944e20b0a853
HwichulShin <shinhwiiron@gmail.com>
2025-01-31 17:04:49 +0900

    ✨ 게시글 상세 페이지 작업
    - 게시글 삭제 기능 추가
    - 댓글 생성, 수정, 삭제 기능 추가
    - 댓글 수정 시 "수정됨" 노출

---
+++ app/boards/[articleId]/ArticleDtail.tsx
@@ -0,0 +1,90 @@
+import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import IconText from '@/components/IconLabel';
+import KebabDropDown from '@/components/KebabDropDown';
+import LikeCount from '@/components/LikeCount';
+import WriterProfile from '@/components/WriterProfile';
+import { dotDate } from '@/utils/dateConversion';
+import { deleteArticle, getArticleDetail } from '@/service/article.api';
+import { GetArticleDetailParams } from '@/types/article.type';
+
+function ArticleDetail({ articleId }: GetArticleDetailParams) {
+  const router = useRouter();
+  const { data } = useQuery({
+    queryKey: ['articleDetail'],
+    queryFn: () => getArticleDetail({ articleId: articleId }),
+  });
+
+  if (!data) return;
+
+  const {
+    title,
+    content,
+    image,
+    createdAt,
+    updatedAt,
+    commentCount,
+    likeCount,
+    writer,
+  } = data;
+
+  const handleArticleDelete = () => {
+    if (confirm('게시글을 삭제하시겠습니까?')) {
+      deleteArticle({ articleId });
+      router.push('/boards');
+    }
+  };
+
+  return (
+    <div className="mt-pr-56">
+      <div className="pb-pr-27.5 pt-pr-24">
+        <div className="flex justify-between">
+          <p className="text-18m">{title}</p>
+          <div className="ml-pr-16 shrink">
+            <KebabDropDown
+              onEdit={() => alert('수정하기')}
+              onDelete={handleArticleDelete}
+            />
+          </div>
+        </div>
+
+        <div className="mt-pr-16 flex justify-between border border-x-0 border-b-0 pt-pr-16">
+          <div className="flex items-center">
+            <WriterProfile writer={writer} />
+            <span className="mx-pr-16 h-pr-12 w-pr-1 bg-b-tertiary" />
+
+            <span className="text-14m text-t-disabled">
+              {dotDate(createdAt)}
+              {createdAt !== updatedAt && ' (수정됨)'}
+            </span>
+          </div>
+
+          <div className="flex">
+            <IconText text={commentCount} type="commentCount" />
+
+            <LikeCount type="interactive" likeCount={likeCount} />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        {image && (
+          <div className="relative mt-pr-24 h-pr-500 w-full">
+            <Image
+              src={image}
+              alt="게시글 이미지"
+              fill
+              className="object-contain"
+            />
+          </div>
+        )}
+
+        <p className="mb-pr-80 mt-pr-24">{content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ArticleDetail;
---
+++ app/boards/[articleId]/CommentContainer.tsx
@@ -0,0 +1,107 @@
+import { ChangeEvent, useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import ArticleCommentTextarea from '@/components/ArticleCommentTextarea/ArticleCommentTextarea';
+import Comment from '@/components/Comment/Comment';
+import {
+  createArticleComment,
+  deleteArticleComment,
+  getArticleCommentList,
+  updateArticleComment,
+} from '@/service/articleComment.api';
+import {
+  CreateArticleCommentParams,
+  DeleteArticleCommentParams,
+  UpdateArticleCommentParams,
+} from '@/types/articleComment.type';
+
+function CommentContainer({ articleId }: { articleId: number }) {
+  const [commentValue, setCommentValue] = useState('');
+
+  const queryClient = useQueryClient();
+
+  // 게시글 댓글 리스트 조회
+  const { data: articleComments } = useQuery({
+    queryKey: ['commentList'],
+    queryFn: () => getArticleCommentList({ articleId, limit: 100 }),
+  });
+
+  const handleCommentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCommentValue(e.target.value);
+  };
+
+  // 게시글 댓글 생성
+  const createCommentMutation = useMutation({
+    mutationFn: (newComment: CreateArticleCommentParams) =>
+      createArticleComment(newComment),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['commentList'] });
+      setCommentValue('');
+    },
+  });
+
+  const handleCommentSubmit = () => {
+    createCommentMutation.mutate({ articleId, content: commentValue });
+  };
+
+  // 게시글 댓글 삭제
+  const deleteCommentMutation = useMutation({
+    mutationFn: (deleteComment: DeleteArticleCommentParams) =>
+      deleteArticleComment(deleteComment),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['commentList'] });
+    },
+  });
+
+  const handleCommentDelete = ({ commentId }: DeleteArticleCommentParams) => {
+    if (confirm('댓글을 삭제하시겠습니까?')) {
+      deleteCommentMutation.mutate({ commentId });
+    }
+  };
+
+  // 게시글 댓글 수정
+  const updateCommentMutation = useMutation({
+    mutationFn: (updateComment: UpdateArticleCommentParams) =>
+      updateArticleComment(updateComment),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['commentList'] });
+    },
+  });
+
+  const handleUpdateSubmit = ({
+    commentId,
+    content,
+  }: UpdateArticleCommentParams) => {
+    if (confirm('댓글을 수정하시겠습니까?')) {
+      updateCommentMutation.mutate({ commentId, content });
+    }
+  };
+
+  if (!articleComments) return null;
+
+  return (
+    <>
+      <ArticleCommentTextarea
+        commentValue={commentValue}
+        handleCommentChange={handleCommentChange}
+        handleCommentSubmit={handleCommentSubmit}
+      />
+
+      <div className="mb-pr-56 mt-pr-40 flex flex-col gap-pr-16 border border-x-0 border-b-0 pt-pr-40">
+        {articleComments.list.map((comment) => (
+          <Comment
+            key={comment.id}
+            type="article"
+            commentData={comment}
+            handleDeleteClick={(id) => handleCommentDelete({ commentId: id })}
+            handleUpdateSubmit={(id, content) =>
+              handleUpdateSubmit({ commentId: id, content })
+            }
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default CommentContainer;
