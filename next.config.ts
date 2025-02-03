e8e1dee2a6c6e5b146f6f9123d84c7390e50a17e
tare <59001439+junghwa1996@users.noreply.github.com>
2025-02-03 20:21:04 +0900

    🔧 next.config.ts에 카카오 도메인 추가
    - 프로필 이미지를 가져오기 위한 도메인

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
       'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
       'ifh.cc',
       'lh3.googleusercontent.com',
+      'k.kakaocdn.net',
     ], // 허용할 도메인 추가
   },
   webpack(config) {
