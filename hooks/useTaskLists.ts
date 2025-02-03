d2d71b324e588189ff47a0fdd2a219f2120e7d3f
WooLegend <dnwogus4260@naver.com>
2025-02-03 18:20:57 +0900

    📝 useTaskLists 주석 작성

--- hooks/useTaskLists.ts
+++ hooks/useTaskLists.ts
@@ -2,6 +2,16 @@ import { getTaskList } from '@/service/taskList.api';
 import useGroupStore from '@/stores/useGroup.store';
 import { useQueries, useQueryClient } from '@tanstack/react-query';
 
+/**
+ * 특정 팀에 속한 모든 할 일 목록을 관리하기 위한 커스텀 훅입니다.
+ *
+ * @property taskList 할 일 목록 상세 리스트
+ * @property isPending 할 일 목록 리스트 요청 중 여부
+ * @property error 요청 중 발생한 에러
+ * @property refetchById 특정 할 일 목록 리페칭
+ * @property removeById 특정 할 일 목록 캐싱 삭제
+ * @property refetchAll 모든 할 일 목록 리페칭
+ */
 export default function useTaskLists() {
   const queryClient = useQueryClient();
   const { taskLists, setTaskLists } = useGroupStore();
