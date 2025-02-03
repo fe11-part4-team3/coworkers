a6b16bc9fb2d44b5d91c4e2797c049d226da63bd
tare <59001439+junghwa1996@users.noreply.github.com>
2025-02-03 20:30:33 +0900

    🔨 로그아웃 시 카카오 로컬스토리지에 저장된 토큰들 제거

--- components/layout/Header/Headers.tsx
+++ components/layout/Header/Headers.tsx
@@ -13,6 +13,7 @@ import SideNavigation from '@/components/SideNavigation/SideNavigation';
 import useUser from '@/hooks/useUser';
 import { IGroup } from '@/types/group.type';
 import DropDown from '@/components/DropDown';
+import { logoutKakaoAccessStorage } from '@/lib/kakaoStorage';
 
 import Profile from './Profile';
 import Logo from './Logo';
@@ -29,7 +30,11 @@ function Headers() {
     const flag = confirm('로그아웃 하시겠습니까?');
     if (flag) {
       clear();
-      signOut({ redirect: false });
+
+      // STUB 세션 로그아웃
+      signOut();
+      logoutKakaoAccessStorage();
+
       alert('로그아웃 되었습니다.');
     }
   }, [clear]);
@@ -44,7 +49,7 @@ function Headers() {
         onClick: logout,
       },
     ],
-    [clear],
+    [logout],
   );
 
   useEffect(() => {
@@ -55,7 +60,7 @@ function Headers() {
     const group = groups.find((group) => group.id === groupId);
     if (group) setCurrentGroup(group);
     else router.push('/');
-  }, [groupId, groups, isPending]);
+  }, [groupId, groups, isPending, router]);
 
   return (
     <header className="fixed z-40 flex w-full items-center border-b bg-b-secondary transition-all">
