02f38821ebd0ff976b65af45d40e46f7750c57b5
WooLegend <dnwogus4260@naver.com>
2025-02-03 18:57:12 +0900

    🎨 코드 포매팅

--- app/[teamId]/(index)/GroupReports.tsx
+++ app/[teamId]/(index)/GroupReports.tsx
@@ -1,4 +1,5 @@
 import { ITask } from '@/types/task.type';
+
 import TodayProgressChart from './charts/TodayProgressChart';
 
 interface GroupReportsProps {
@@ -9,8 +10,9 @@ export default function GroupReports({ tasks }: GroupReportsProps) {
   return (
     <div>
       <h3>리포트</h3>
-      <div className="flex items-center rounded-pr-12 bg-b-secondary px-pr-24"></div>
-      <TodayProgressChart tasks={tasks} />
+      <div className="flex items-center rounded-pr-12 bg-b-secondary px-pr-24">
+        <TodayProgressChart tasks={tasks} />
+      </div>
     </div>
   );
 }
