69f735baadd0a13fcc26c5fd5783519018a71b6d
WooLegend <dnwogus4260@naver.com>
2025-02-05 02:52:24 +0900

    🔨 리포트 컴포넌트 마크업

--- app/[teamId]/(index)/GroupReports.tsx
+++ app/[teamId]/(index)/GroupReports.tsx
@@ -1,17 +1,25 @@
 import { ITask } from '@/types/task.type';
+import { ITaskList } from '@/types/taskList.type';
 
 import TodayProgressChart from './charts/TodayProgressChart';
+import TodayTasksChart from './charts/TodayTasksChart';
+import { Separator } from '@/components/ui/separator';
 
 interface GroupReportsProps {
   tasks: ITask[];
+  taskLists: ITaskList[] | null;
 }
 
-export default function GroupReports({ tasks }: GroupReportsProps) {
+export default function GroupReports({ tasks, taskLists }: GroupReportsProps) {
   return (
     <div>
-      <h3>리포트</h3>
-      <div className="flex items-center rounded-pr-12 bg-b-secondary px-pr-24">
+      <h3 className="mb-pr-16">리포트</h3>
+      <div className="flex h-pr-250 items-center justify-around rounded-pr-12 bg-b-secondary p-pr-24">
         <TodayProgressChart tasks={tasks} />
+        <Separator orientation="vertical" />
+        <TodayTasksChart state="todo" taskLists={taskLists || []} />
+        <Separator orientation="vertical" />
+        <TodayTasksChart state="done" taskLists={taskLists || []} />
       </div>
     </div>
   );
