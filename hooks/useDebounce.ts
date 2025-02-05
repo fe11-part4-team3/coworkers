5098348ddd8bad6f54196e069f9c3f8f06ae6fdf
tare <59001439+junghwa1996@users.noreply.github.com>
2025-02-06 04:21:12 +0900

    🔨 useDebounce 훅 수정
    - useEffect로 callback이 변경될 때마다 callbackRef.current = callback을 갱신하는 방식으로 변경

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,20 +1,25 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useEffect } from 'react';
 
 function useDebounce(callback: (...args: any[]) => void, delay: number) {
   const callbackRef = useRef(callback);
-  const timeoutRef = useRef<NodeJS.Timeout>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  // callback이 변경될 때마다 최신 콜백을 갱신
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
 
   return useCallback(
     (...args: any[]) => {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
       }
-      timeoutRef.current = setTimeout(
-        () => callbackRef.current(...args),
-        delay,
-      );
+      timeoutRef.current = setTimeout(() => {
+        callbackRef.current(...args);
+      }, delay);
     },
-    [delay, callback],
+    [delay],
   );
 }
+
 export default useDebounce;
