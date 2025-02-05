4443d696b7ecd4e594185e3aeb67a47c9e71a66e
kimgwanho0206@gmail.com <kimgwanho0206@gmail.com>
2025-02-05 23:07:22 +0900

    메인 첫번째 박스 작업

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Container from '@/components/layout/Container';
 import Buttons from '@/components/Buttons';
 import useUser from '@/hooks/useUser';
 import { useRouter } from 'next/navigation';
-import IconBox from '@/components/Landing/iconbox';
+import IconBox from '@/components/Landing/Iconbox';
 
 export default function LandingPage() {
   const router = useRouter();
@@ -77,7 +77,7 @@ export default function LandingPage() {
 
       {/* 두 번째 섹션 (컨텐츠 박스) */}
       <section className="relative flex flex-col items-center gap-pr-80">
-        <div className="relative mt-pr-60 h-pr-419 w-pr-996 rounded-pr-40 bg-gradient-to-r from-brand-primary to-brand-tertiary p-pr-1">
+        <div className="relative mt-pr-60 h-pr-419 w-pr-996 rounded-pr-40 bg-gradient-to-r from-brand-primary to-brand-tertiary p-pr-1 mo:h-pr-487 mo:w-pr-343 ta:h-pr-354 ta:w-pr-696">
           <div className="relative flex h-full w-full rounded-pr-40 bg-b-primary shadow-lg backdrop-blur-md">
             {/* 이미지 */}
             <Image
@@ -85,15 +85,15 @@ export default function LandingPage() {
               alt="Mockup 1"
               width={291}
               height={338}
-              className="absolute bottom-0 left-pr-174 h-pr-338 w-pr-291 object-contain"
+              className="absolute bottom-0 left-pr-174 h-pr-338 w-pr-291 object-contain mo:bottom-0 mo:left-1/2 mo:h-pr-273 mo:w-pr-235 mo:-translate-x-1/2 ta:left-pr-120"
             />
 
-            {/* 아이콘텍스트박스 */}
-            <div className="absolute right-pr-181 top-pr-155 h-pr-120 w-pr-157">
+            {/* 아이콘텍스트 박스 */}
+            <div className="absolute right-pr-181 top-pr-155 h-pr-120 w-pr-157 mo:right-pr-54 mo:top-pr-48 mo:h-pr-106 mo:w-pr-118 ta:right-pr-121 ta:top-pr-124 ta:h-pr-106 ta:w-pr-118">
               <IconBox imageUrl="foldericon.svg" alt="Folder Icon" left />
 
               {/* 텍스트 */}
-              <p className="absolute bottom-0 whitespace-nowrap text-24m leading-none text-t-primary">
+              <p className="absolute bottom-0 whitespace-nowrap text-24m leading-none text-t-primary mo:text-18m">
                 그룹으로
                 <br />할 일을 관리해요
               </p>
