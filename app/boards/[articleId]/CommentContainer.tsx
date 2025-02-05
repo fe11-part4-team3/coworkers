a2c4fadbf177a25c431874e96d5527f863c5f944
HwichulShin <shinhwiiron@gmail.com>
2025-02-05 19:19:54 +0900

    ✨ 무한스크롤 기능 추가

--- app/boards/[articleId]/CommentContainer.tsx
+++ app/boards/[articleId]/CommentContainer.tsx
@@ -1,5 +1,10 @@
-import { ChangeEvent, useState } from 'react';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { ChangeEvent, useEffect, useState } from 'react';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
 import ArticleCommentTextarea from '@/components/ArticleCommentTextarea/ArticleCommentTextarea';
 import Comment from '@/components/Comment/Comment';
@@ -19,16 +24,29 @@ function CommentContainer({ articleId }: { articleId: number }) {
   const [commentValue, setCommentValue] = useState('');
 
   const queryClient = useQueryClient();
+  const { ref, inView } = useInView();
 
-  // 게시글 댓글 리스트 조회
+  // 게시글 댓글 리스트 조회 (무한스크롤)
   const {
     data: articleComments,
+    fetchNextPage, // 다음 페이지 데이터를 가져오는 함수
+    hasNextPage, // 다음 페이지가 있는지 여부
     isLoading,
     isError,
-  } = useQuery({
+  } = useInfiniteQuery({
     queryKey: ['commentList', articleId],
-    queryFn: () => getArticleCommentList({ articleId, limit: 100 }),
+    queryFn: ({ pageParam = null }: { pageParam: number | null }) =>
+      getArticleCommentList({ articleId, limit: 6, cursor: pageParam }),
+    initialPageParam: null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor ?? undefined, // 다음 페이지의 커서 반환
   });
+
+  useEffect(() => {
+    // 센서가 감지되고, 다음 페이지가 있다면 데이터 요청
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   const handleCommentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setCommentValue(e.target.value);
@@ -93,24 +111,29 @@ function CommentContainer({ articleId }: { articleId: number }) {
       />
 
       <div className="mb-pr-56 mt-pr-40 flex flex-col gap-pr-16 border border-x-0 border-b-0 pt-pr-40">
-        {articleComments.list.length > 0 ? (
-          articleComments.list.map((comment) => (
-            <Comment
-              key={comment.id}
-              type="article"
-              commentData={comment}
-              handleDeleteClick={(id) => handleCommentDelete({ commentId: id })}
-              handleUpdateSubmit={(id, content) =>
-                handleUpdateSubmit({ commentId: id, content })
-              }
-              isLoading={isLoading}
-            />
-          ))
+        {articleComments?.pages[0].list.length > 0 ? (
+          articleComments.pages
+            .flatMap((page) => page.list) // 페이지별 댓글 리스트를 하나의 배열로 변환
+            .map((comment) => (
+              <Comment
+                key={comment.id}
+                type="article"
+                commentData={comment}
+                handleDeleteClick={(id) =>
+                  handleCommentDelete({ commentId: id })
+                }
+                handleUpdateSubmit={(id, content) =>
+                  handleUpdateSubmit({ commentId: id, content })
+                }
+                isLoading={isLoading}
+              />
+            ))
         ) : (
           <p className="my-pr-118 text-center text-16m text-t-default">
             아직 작성된 댓글이 없습니다.
           </p>
         )}
+        <div ref={ref} style={{ height: '20px' }} />
       </div>
     </>
   );
