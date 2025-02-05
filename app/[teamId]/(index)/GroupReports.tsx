002529dc03218b67ce5d2e6ae03dbd68918eb5c4
WooLegend <dnwogus4260@naver.com>
2025-02-05 14:48:01 +0900

    💄 할 일/한 일 차트 캐러셀 적용

--- app/[teamId]/(index)/GroupReports.tsx
+++ app/[teamId]/(index)/GroupReports.tsx
@@ -6,6 +6,14 @@ import { useDeviceType } from '@/contexts/DeviceTypeContext';
 import TodayTasksChart from './charts/TodayTasksChart';
 import TodayProgressChart from './charts/TodayProgressChart';
 
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel';
+import { useEffect, useState } from 'react';
+
 interface GroupReportsProps {
   tasks: ITask[];
   taskLists: ITaskList[] | null;
@@ -39,13 +47,44 @@ function DefaultContent({ tasks, taskLists }: GroupReportsProps) {
 }
 
 function MobileContent({ tasks, taskLists }: GroupReportsProps) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
+
   return (
-    <div className="flex h-pr-250 items-center justify-around rounded-pr-12 bg-b-secondary p-pr-24 xmo:h-fit xmo:flex-col">
+    <div className="flex h-pr-250 select-none items-center justify-around rounded-pr-12 bg-b-secondary p-pr-24 xmo:h-fit xmo:flex-col">
       <TodayProgressChart tasks={tasks} />
       <Separator orientation="horizontal" className="my-4 hidden xmo:block" />
       <Separator orientation="vertical" className="xmo:hidden" />
-      <TodayTasksChart state="todo" taskLists={taskLists || []} />
-      <TodayTasksChart state="done" taskLists={taskLists || []} />
+      <div>
+        <Carousel className="w-full max-w-pr-200" setApi={setApi}>
+          <CarouselContent>
+            <CarouselItem>
+              <TodayTasksChart state="todo" taskLists={taskLists || []} />
+            </CarouselItem>
+            <CarouselItem>
+              <TodayTasksChart state="done" taskLists={taskLists || []} />
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
+        <div className="flex justify-center gap-pr-8">
+          <div
+            className={`size-pr-12 rounded-full transition-all duration-300 ${current ? 'bg-b-tertiary' : 'bg-t-primary'}`}
+          />
+          <div
+            className={`size-pr-12 rounded-full transition-all duration-300 ${current ? 'bg-t-primary' : 'bg-b-tertiary'}`}
+          />
+        </div>
+      </div>
     </div>
   );
 }
