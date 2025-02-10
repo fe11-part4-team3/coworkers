6ff91107eefb41f56680a663a6493da7079a495d
WooLegend <dnwogus4260@naver.com>
2025-02-11 00:11:44 +0900

    📝 useThemeMode 주석 추가

--- hooks/useThemeMode.ts
+++ hooks/useThemeMode.ts
@@ -5,6 +5,20 @@ const DEFAULT_THEME = 'dark';
 
 export type Theme = 'dark' | 'light';
 
+/**
+ * `useTheme`의 쉽게 사용하기 위한 커스텀 훅
+ *
+ * 기존 `theme`은 `string`타입에 `dark`, `light`, `system`으로 나뉘어 있습니다.
+ *
+ * 만약 `system` 테마일 경우 `dark`인지 `light`인지 한 번 더 확인해야 해서 불편합니다.
+ *
+ * 그래서 해당 훅은 추가적인 작업 없이 `dark`인지 `light`인지 바로 반환해줍니다.
+ *
+ * 추가적으로 토글 핸들러를 제공해 모드 전환도 간단히 할 수 있습니다.
+ *
+ * @property theme : 테마. `dark` 혹은 `light`
+ * @property toggleTheme : 토글 핸들러. `dark`->`light`, `light`->`dark`로 전환
+ */
 export default function useThemeMode() {
   const [currentTheme, setCurrentTheme] = useState<Theme>(DEFAULT_THEME);
   const { theme, systemTheme, setTheme } = useTheme();
