2026843cc4b6eaaade39d23e92fbed614a741c14
HwichulShin <shinhwiiron@gmail.com>
2025-02-11 22:18:07 +0900

    📝 할 일 카드(마이 히스토리) toast 제거 후 스낵바 적용

--- components/TaskCard/TaskCheckbox.tsx
+++ components/TaskCard/TaskCheckbox.tsx
@@ -1,6 +1,4 @@
 import { TaskCheckboxProps } from '@/types/taskCard.type';
-import { useToast } from '@/hooks/use-toast';
-import { Toaster } from '@/components/ui/toaster';
 
 /**
  * @param {string} props.name - 할 일 이름
@@ -15,41 +13,25 @@ function TaskCheckbox({
   handleCheckedToggle,
   isTaskList,
 }: TaskCheckboxProps) {
-  const { toast } = useToast();
-
-  const handleClick = () => {
-    if (!isTaskList) {
-      toast({
-        variant: 'destructive',
-        description:
-          '할 일 완료 처리는 "할 일 리스트" 페이지에서만 가능합니다.',
-      });
-    }
-  };
-
   return (
-    <>
-      <div className="flex gap-pr-12" onClick={handleClick}>
-        <input
-          id={name}
-          type="checkbox"
-          className={`size-pr-16 shrink-0 appearance-none rounded-md border border-t-primary checked:border-none checked:bg-[url('/images/icon-checked.svg')] ${isTaskList && 'cursor-pointer'}`}
-          checked={isChecked}
-          onChange={handleCheckedToggle}
-        />
+    <div className="flex gap-pr-12">
+      <input
+        id={name}
+        type="checkbox"
+        className={`size-pr-16 shrink-0 appearance-none rounded-md border border-t-primary checked:border-none checked:bg-[url('/images/icon-checked.svg')] ${isTaskList && 'cursor-pointer'}`}
+        checked={isChecked}
+        onChange={handleCheckedToggle}
+      />
 
-        <div className="grid">
-          <label
-            htmlFor={name}
-            className={`truncate text-14m font-normal leading-4 ${isChecked && 'line-through'} ${isTaskList && 'cursor-pointer'}`}
-          >
-            {name}
-          </label>
-        </div>
+      <div className="grid">
+        <label
+          htmlFor={name}
+          className={`truncate text-14m font-normal leading-4 ${isChecked && 'line-through'} ${isTaskList && 'cursor-pointer'}`}
+        >
+          {name}
+        </label>
       </div>
-
-      <Toaster />
-    </>
+    </div>
   );
 }
 
