2817ddd86689b65f3d7738d3ae697c98471691f6
WooLegend <dnwogus4260@naver.com>
2025-02-14 03:04:15 +0900

    💄 팀 추가하기 모달 디자인 개선

--- components/NavigationGroupDropdown/AddTeamModal.tsx
+++ components/NavigationGroupDropdown/AddTeamModal.tsx
@@ -27,9 +27,13 @@ export default function AddTeamModal() {
         <h2 className="modal-title">팀 추가하기</h2>
       </div>
       <div className="modal-button-wrapper">
-        <Buttons text="팀 생성하기" onClick={handleClickAdd} />
-      </div>
-      <div className="modal-button-wrapper">
+        <Buttons
+          text="팀 생성하기"
+          onClick={handleClickAdd}
+          backgroundColor="none"
+          border="primary"
+          textColor="primary"
+        />
         <Buttons text="팀 참여하기" onClick={handleClickJoin} />
       </div>
     </>
