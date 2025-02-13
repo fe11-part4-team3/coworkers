f29c496bb1379e912747fc604b5cacc1236b378a
WooLegend <dnwogus4260@naver.com>
2025-02-14 03:56:47 +0900

    🔨 할 일 목록 카드에 개선된 KebabDropDown 적용

--- app/[teamId]/(index)/GroupTaskList.tsx
+++ app/[teamId]/(index)/GroupTaskList.tsx
@@ -4,7 +4,6 @@ import { MouseEvent, useRef } from 'react';
 import { ITaskList } from '@/types/taskList.type';
 import { RoleType } from '@/types/group.type';
 import createUrlString from '@/utils/createUrlString';
-import DropDown from '@/components/DropDown';
 import useModalStore from '@/stores/modalStore';
 
 import { _DeleteTaskListParams, _UpdateTaskListParams } from './TeamPage.type';
@@ -12,6 +11,7 @@ import { PointColorType } from './GroupTaskListWrapper';
 import TaskProgressBadge from './TaskProgressBadge';
 import EditTaskListModal from './EditTaskListModal';
 import DeleteTaskListModal from './DeleteTaskListModal';
+import KebabDropDown from '@/components/KebabDropDown';
 
 type IPointColorClasses = {
   [key in PointColorType]: string;
@@ -79,17 +79,11 @@ export default function GroupTaskList({
         <div className="flex items-center gap-pr-4">
           <TaskProgressBadge tasks={taskList.tasks} />
           {role === 'ADMIN' && (
-            <div className="size-pr-16">
-              <DropDown
-                ref={dropdownRef}
-                trigger={<button className="icon-kebab absolute" />}
-                items={[
-                  { text: '수정하기', onClick: handleClickEdit },
-                  { text: '삭제하기', onClick: handleClickDelete },
-                ]}
-                width="w-pr-120"
-              />
-            </div>
+            <KebabDropDown
+              ref={dropdownRef}
+              onEdit={handleClickEdit}
+              onDelete={handleClickDelete}
+            />
           )}
         </div>
       </div>
