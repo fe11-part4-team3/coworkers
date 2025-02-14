a19f8b91d3390a0c70704a378266d676b359e8a7
tare <siltarre@gmail.com>
2025-02-14 20:38:49 +0900

    🔨 최종렌더링 컴포넌트에서 pathname 관리, custom 삭제
    - custom을 쓰니까 한번 수정할때, custom도 같이 수정해줘야해서 번거롭기때문에  삭제

--- components/SideNavigation/SideNavigation.tsx
+++ components/SideNavigation/SideNavigation.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import { IGroup } from '@/types/group.type';
 
@@ -26,37 +26,13 @@ interface SNBProps {
 }
 
 /**
- * STUB 사이드 네비게이션 컴포넌트 커스텀
- * @description 사이드 네비게이션 컴포넌트를 shadcn 컴포넌트와 구분을 위해 사용
- */
-const Custom = {
-  SideNavGroupLabel: ({ toggleSidebar }: { toggleSidebar: () => void }) => (
-    <SideNavGroupLabel toggleSidebar={toggleSidebar} />
-  ),
-  SideNavTeamList: ({
-    groups,
-    handleClick,
-  }: {
-    groups: IGroup[] | null;
-    handleClick: (path: string) => void;
-  }) => <SideNavTeamList groups={groups} handleClick={handleClick} />,
-  SideNavDefaultList: ({
-    handleClick,
-  }: {
-    handleClick: (path: string) => void;
-  }) => <SideNavDefaultList handleClick={handleClick} />,
-  SideNavigationTrigger: ({ src, alt }: { src: string; alt: string }) => (
-    <SideNavigationTrigger src={src} alt={alt} />
-  ),
-};
-
-/**
  * STUB 사이드 네비게이션 최종 랜더링
  * @param props
  * @param props.groups 그룹 배열
  */
 function SideNavigation({ groups }: SNBProps) {
   const router = useRouter();
+  const pathname = usePathname();
   const { toggleSidebar } = useSidebar();
 
   const handleClick = (path: string) => {
@@ -68,21 +44,25 @@ function SideNavigation({ groups }: SNBProps) {
     <div className="fixed left-0 top-0 z-10">
       <Sidebar className="border-none">
         <SidebarHeader className="items-end p-pr-16">
-          <Custom.SideNavigationTrigger
+          <SideNavigationTrigger
             src="images/icon-close.svg"
             alt="사이드 네비게이션 닫기"
           />
         </SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
-            <Custom.SideNavGroupLabel toggleSidebar={toggleSidebar} />
+            <SideNavGroupLabel toggleSidebar={toggleSidebar} />
             <SidebarGroupContent>
               <SidebarMenu>
-                <Custom.SideNavTeamList
+                <SideNavTeamList
                   groups={groups}
                   handleClick={handleClick}
+                  pathname={pathname}
                 />
-                <Custom.SideNavDefaultList handleClick={handleClick} />
+                <SideNavDefaultList
+                  handleClick={handleClick}
+                  pathname={pathname}
+                />
               </SidebarMenu>
             </SidebarGroupContent>
           </SidebarGroup>
