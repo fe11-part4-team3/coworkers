088ff3b61202d66350399840b71061b2ed85891f
tare <siltarre@gmail.com>
2025-02-15 02:50:21 +0900

    🔨 데이피커 선택된 날짜 부모로 반환 기능 추가 및 위치 조정

--- components/DateTimePicker/DatePicker.tsx
+++ components/DateTimePicker/DatePicker.tsx
@@ -5,47 +5,44 @@ import { format } from 'date-fns';
 
 import { Calendar } from '@/components/ui/calendar';
 import InputField from '@/components/InputField/InputField';
-import { widthStyledSliceWPr } from '@/utils/filterClass';
 
 /**
  * DatePicker 컴포넌트는 달력을 표시하고 선택한 날짜를 반환합니다.
- * @param {string} width - optional, DatePicker 컴포넌트의 너비를 설정합니다. (기본 값은 w-full입니다.)
- * @returns {JSX.Element} DatePicker 컴포넌트를 반환합니다.
+ * @param {Function} onDateChange - 날짜 변경 시 호출되는 함수
  */
-
-export default function DatePicker({ width }: { width?: string }) {
+export default function DatePicker({
+  onDateChange,
+}: {
+  onDateChange: (date: Date) => void;
+}) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [date, setdate] = useState<Date | undefined>(undefined);
-
-  const buttonStyle = isOpen ? 'border border-brand-primary' : '';
+  const [date, setdate] = useState<Date>(new Date());
 
   const formattedDate = date ? format(date, 'yyyy년 MM월 dd일') : '';
 
+  const handleDateSelect = (selectedDate: Date) => {
+    setdate(selectedDate);
+    onDateChange(selectedDate);
+  };
+
   return (
     <>
-      <div
-        className={`${width ? widthStyledSliceWPr(width) : 'w-full'} flex flex-col gap-pr-8`}
-      >
-        <div className="relative">
-          <InputField
-            value={formattedDate}
-            placeholder="날짜를 선택해주세요."
-            disabled={true}
-            onChange={() => {}}
-            name="date"
-          />
-          <button
-            className={`absolute left-0 top-0 z-20 size-full rounded-xl ${buttonStyle}`}
-            onClick={() => setIsOpen(!isOpen)}
-          />
-        </div>
+      <div className="relative flex w-full flex-col gap-pr-8">
+        <InputField
+          value={formattedDate}
+          name="date"
+          onClick={() => setIsOpen(!isOpen)}
+          readOnly
+        />
         {isOpen && (
           <Calendar
             mode="single"
             selected={date}
-            onSelect={setdate}
+            onSelect={(selectedDate: Date | undefined) => {
+              if (selectedDate) handleDateSelect(selectedDate);
+            }}
             onDayClick={() => setIsOpen(false)}
-            className="flex w-full items-center justify-center rounded-2xl border border-brand-primary bg-b-secondary p-pr-16"
+            className="absolute left-0 top-pr-60 z-10 flex min-h-pr-258 w-full items-center justify-center rounded-2xl border border-brand-primary bg-b-secondary p-pr-16"
           />
         )}
       </div>
